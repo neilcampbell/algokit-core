2f8f6ae178be3474868380e8d13710a399747a82
Joe Polny <joepolny@gmail.com>
2024-12-15 14:57:54 -0500

    sign with noble ed25519

--- tests/js/index.ts
+++ tests/js/index.ts
@@ -5,6 +5,7 @@ import init, {
 } from "./pkg/algo_models_rs";
 
 let { AlgorandTransactionCrafter } = require("@algorandfoundation/algo-models");
+import * as ed from "@noble/ed25519";
 
 import * as msgpack from "algo-msgpack-with-bigint";
 
@@ -30,6 +31,8 @@ async function main() {
   // After msgpack encoding a TX TAG is added as a prefix to the result.
   const encodedTs: Uint8Array = tx.encode(); // encoded msg ready - to be signed with EdDSA
 
+  // Now for the Rust version...
+
   const fields = {
     header: {
       sender: tx.snd,
@@ -44,24 +47,24 @@ async function main() {
     amount: tx.amt,
   } as PayTransactionFields;
 
-  const encodedRs = encodePayment(fields, true);
+  const btyesForSigning = encodePayment(fields, true);
 
+  // Signing with a ed25519 lib that has no idea about Algorand
+  const privKey = ed.utils.randomPrivateKey();
+  const sig = await ed.signAsync(btyesForSigning, privKey);
+  const signedTx = attachSignature(btyesForSigning, sig);
+
+  console.log("signedTx", msgpack.decode(signedTx));
   console.log("algo-models-ts", encodedTs);
-  console.log("algo-models-rs", encodedRs);
-  console.log("algo-models-rs", msgpack.decode(encodedRs.slice(2)));
+  console.log("algo-models-rs", btyesForSigning);
+  console.log("algo-models-rs", msgpack.decode(btyesForSigning.slice(2)));
   console.log("algo-models-ts", msgpack.decode(encodedTs.slice(2)));
 
-  if (encodedTs.toString() !== encodedRs.toString()) {
+  if (encodedTs.toString() !== btyesForSigning.toString()) {
     throw new Error("Encoded transactions are not equal");
   } else {
     console.log("Encoded transactions are equal");
   }
-
-  const btyesForSigning = encodePayment(fields, true);
-  const sig = new Uint8Array(64);
-  const signedTx = attachSignature(btyesForSigning, sig);
-
-  console.log("signedTx", msgpack.decode(signedTx));
 }
 
 main();
