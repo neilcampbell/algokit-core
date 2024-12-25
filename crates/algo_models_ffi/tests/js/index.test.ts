5d4959d03c1daa903126434768df7686a067743a
Joe Polny <joepolny@gmail.com>
2024-12-25 14:42:28 -0500

    load test data from common js file

--- crates/algo_models_ffi/tests/js/index.test.ts
+++ crates/algo_models_ffi/tests/js/index.test.ts
@@ -1,4 +1,4 @@
-import { expect, test, beforeAll, describe, beforeEach } from "bun:test";
+import { expect, test, describe } from "bun:test";
 import * as ed from "@noble/ed25519";
 import init, {
   type PayTransactionFields,
@@ -6,78 +6,37 @@ import init, {
   attachSignature,
   decodePayment,
   getEncodedTransactionType,
-  TransactionType,
 } from "./pkg/algo_models_ffi";
+import path from "path";
 
 describe("algo_models WASM", async () => {
-  const privKey: Uint8Array = new Uint8Array([
-    228, 37, 78, 229, 20, 67, 165, 234, 193, 70, 211, 78, 7, 165, 168, 197, 144,
-    243, 236, 114, 114, 147, 229, 16, 106, 15, 230, 26, 218, 8, 39, 3,
-  ]);
+  await init();
 
-  const getHeader = (transactionType: TransactionType) => {
-    return {
-      sender: new Uint8Array([
-        154, 33, 203, 60, 169, 134, 36, 253, 73, 75, 144, 135, 105, 171, 72, 68,
-        33, 226, 186, 94, 9, 240, 140, 12, 209, 166, 101, 40, 185, 34, 235, 246,
-      ]),
-      fee: 1000n,
-      transactionType,
-      firstValid: 1000n,
-      lastValid: 1500n,
-      genesisHash: new Uint8Array([
-        72, 99, 181, 24, 164, 179, 200, 78, 200, 16, 242, 45, 79, 16, 129, 203,
-        15, 113, 240, 89, 167, 172, 32, 222, 198, 47, 127, 112, 229, 9, 58, 34,
-      ]),
-      genesisId: "testnet-v1.0",
-    };
-  };
+  const jsonString = await Bun.file(
+    path.join(__dirname, "../test_data.json")
+  ).text();
 
-  beforeAll(async () => {
-    await init();
+  const testData = JSON.parse(jsonString, (_, value) => {
+    if (Array.isArray(value) && value.every((n) => typeof n === "number")) {
+      return new Uint8Array(value);
+    }
+
+    if (
+      typeof value === "number" &&
+      ["fee", "amount", "firstValid", "lastValid"].includes(_)
+    ) {
+      return BigInt(value);
+    }
+
+    return value;
   });
 
+  const privKey = testData.privKey;
+
   describe("payment", () => {
-    const fields: PayTransactionFields = {
-      header: getHeader("Payment"),
-      receiver: new Uint8Array([
-        247, 150, 167, 185, 149, 130, 87, 172, 85, 147, 5, 104, 184, 87, 44,
-        206, 68, 62, 179, 88, 173, 129, 245, 68, 228, 113, 240, 228, 211, 170,
-        89, 244,
-      ]),
-      amount: 1000000n,
-    };
-    const expectedSignedTxn = new Uint8Array([
-      130, 163, 115, 105, 103, 196, 64, 110, 97, 3, 133, 117, 193, 222, 221,
-      115, 216, 59, 216, 123, 232, 226, 237, 220, 171, 1, 51, 24, 243, 49, 148,
-      31, 181, 185, 71, 7, 169, 76, 183, 120, 29, 137, 177, 17, 145, 94, 113,
-      167, 18, 0, 78, 232, 191, 71, 207, 8, 173, 81, 111, 210, 48, 201, 198,
-      136, 107, 186, 3, 207, 102, 192, 3, 163, 116, 120, 110, 137, 163, 97, 109,
-      116, 206, 0, 15, 66, 64, 163, 102, 101, 101, 205, 3, 232, 162, 102, 118,
-      205, 3, 232, 163, 103, 101, 110, 172, 116, 101, 115, 116, 110, 101, 116,
-      45, 118, 49, 46, 48, 162, 103, 104, 196, 32, 72, 99, 181, 24, 164, 179,
-      200, 78, 200, 16, 242, 45, 79, 16, 129, 203, 15, 113, 240, 89, 167, 172,
-      32, 222, 198, 47, 127, 112, 229, 9, 58, 34, 162, 108, 118, 205, 5, 220,
-      163, 114, 99, 118, 196, 32, 247, 150, 167, 185, 149, 130, 87, 172, 85,
-      147, 5, 104, 184, 87, 44, 206, 68, 62, 179, 88, 173, 129, 245, 68, 228,
-      113, 240, 228, 211, 170, 89, 244, 163, 115, 110, 100, 196, 32, 154, 33,
-      203, 60, 169, 134, 36, 253, 73, 75, 144, 135, 105, 171, 72, 68, 33, 226,
-      186, 94, 9, 240, 140, 12, 209, 166, 101, 40, 185, 34, 235, 246, 164, 116,
-      121, 112, 101, 163, 112, 97, 121,
-    ]);
-    const expectedBytesForSigning = new Uint8Array([
-      84, 88, 137, 163, 97, 109, 116, 206, 0, 15, 66, 64, 163, 102, 101, 101,
-      205, 3, 232, 162, 102, 118, 205, 3, 232, 163, 103, 101, 110, 172, 116,
-      101, 115, 116, 110, 101, 116, 45, 118, 49, 46, 48, 162, 103, 104, 196, 32,
-      72, 99, 181, 24, 164, 179, 200, 78, 200, 16, 242, 45, 79, 16, 129, 203,
-      15, 113, 240, 89, 167, 172, 32, 222, 198, 47, 127, 112, 229, 9, 58, 34,
-      162, 108, 118, 205, 5, 220, 163, 114, 99, 118, 196, 32, 247, 150, 167,
-      185, 149, 130, 87, 172, 85, 147, 5, 104, 184, 87, 44, 206, 68, 62, 179,
-      88, 173, 129, 245, 68, 228, 113, 240, 228, 211, 170, 89, 244, 163, 115,
-      110, 100, 196, 32, 154, 33, 203, 60, 169, 134, 36, 253, 73, 75, 144, 135,
-      105, 171, 72, 68, 33, 226, 186, 94, 9, 240, 140, 12, 209, 166, 101, 40,
-      185, 34, 235, 246, 164, 116, 121, 112, 101, 163, 112, 97, 121,
-    ]);
+    const fields: PayTransactionFields = testData.fields;
+    const expectedSignedTxn = testData.expectedSignedTxn;
+    const expectedBytesForSigning = testData.expectedBytesForSigning;
 
     test("encode", () => {
       expect(encodePayment(fields)).toEqual(expectedBytesForSigning);
