35ff80842c230f33f4594cc6da6beca8a7b9618f
Joe Polny <joepolny@gmail.com>
2025-04-29 16:05:37 -0400

    ci(python): getConfig function
    The main idea is that we have a single function that is used to
    configure all of the releases in this monorepo ensuring that there is no
    disrepency between languages/packages

---
+++ utils/semantic-release.cjs
@@ -0,0 +1,48 @@
+module.exports = {
+  /**
+   * @param opts {{
+   *    language: string;
+   *    package_name: string;
+   *    assets?: string[];
+   * }}
+   *
+   * @returns {import('semantic-release').GlobalConfig}
+   */
+  getConfig: (opts) => {
+    const { language, package_name } = opts;
+    const assets = opts.assets || [];
+    return {
+      branches: [{ name: "main", prerelease: "alpha" }],
+      repositoryUrl: "https://github.com/algorandfoundation/algokit-core",
+      tagFormat: `${language}/${package_name}` + "@${version}",
+      plugins: [
+        [
+          "semantic-release-scope-filter",
+          {
+            scopes: [
+              language, // A change to the language's build process
+              `${language}/${package_name}`, // A change that only affects a specific package for a specific langauge
+              package_name, // A change made to non-ffi crate
+              `${package_name}_ffi`, // A change made to ffi crate
+            ],
+            filterOutMissingScope: false, // Assume any commit without a scope affects this package
+          },
+        ],
+        [
+          "@semantic-release/commit-analyzer",
+          {
+            preset: "angular",
+          },
+        ],
+        "@semantic-release/release-notes-generator",
+        [
+          "@semantic-release/github",
+          {
+            assets,
+          },
+        ],
+        "semantic-release-gha-output",
+      ],
+    };
+  },
+};
